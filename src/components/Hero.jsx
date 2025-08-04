0d937268eb73d82ba739353333fa21064cee8680
Sanjay Rajoriya <rajoriya654@gmail.com>
2025-08-04 13:20:58 +0530

    Hero Scroll animation leaf position update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
             }
         })
         .to('.right-leaf', { y: 200}, 0)
-        .to('.left-leaf', {y: 200}, 0)
+        .to('.left-leaf', {y: -200}, 0)
 
         const startValue = isMobile ? 'top 50%' : 'center 60%';
         const endValue = isMobile ? '120% top' : 'bottom top';
