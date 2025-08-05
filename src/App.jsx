8e52ef1a0f9ea3cfac8c42d51daabc9eccb0e975
Sanjay Rajoriya <rajoriya654@gmail.com>
2025-08-06 01:05:26 +0530

    Implement contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cocktails from './components/Cocktails.jsx';
 import About from './components/About.jsx';
 import Art from './components/Art.jsx';
 import { Menu } from './components/Menu.jsx';
+import Contact from './components/Contact.jsx';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText); // Register the plugins
@@ -20,6 +21,7 @@ const App = () => {
     <About />
     <Art />
     <Menu />
+    <Contact />
    </main>
   )
 }
